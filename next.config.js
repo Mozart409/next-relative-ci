c7931cb819049fe90c5c0622ad2c143ba0574963
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-07-28 13:34:55 +0200

    ci: :construction_worker: added relative ci

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const { StatsWriterPlugin } = require('webpack-stats-plugin')
+
 module.exports = {
   reactStrictMode: true,
+  webpack5: true,
+  webpack: (config, {}) => {
+    config.plugins.push(
+      new StatsWriterPlugin({
+        filename: 'stats.json',
+        stats: {
+          context: './', // optional, will improve readability of the paths
+          assets: true,
+          entrypoints: true,
+          chunks: true,
+          modules: true,
+        },
+      })
+    )
+
+    return config
+  },
 }
